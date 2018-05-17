a1668f622a304225fe2dc1564eaebb18254df59d
ocknamo <s14pes@gmail.com>
2018-05-17 19:22:06 +0900

    データベースの指定方法をsequelizeの推奨方法に変更した

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,8 @@
 'use strict';
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:postgres@localhost/tipnem_faucet',
-{ logging: console.log });
+const sequelize = new Sequelize('tipnem_faucet', 'postgres', '', {
+  dialect: 'postgres',
+  logging: console.log });
 
 module.exports = {
   database: sequelize,
