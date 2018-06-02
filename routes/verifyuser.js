749815ad4b8417f8815c264038db000852bc715a
ocknamo <s14pes@gmail.com>
2018-06-02 23:56:23 +0900

    '24時間以内にリクエストを行っていたため検証修了'のlogが24時間以内にリクエストを行って[いない場合でも表示されてしまうBUGを修正した

--- routes/verifyuser.js
+++ routes/verifyuser.js
@@ -29,7 +29,7 @@ const VerifyUser = function (tweetJSON, requestAt) {
 
       if (tweetJSON.user.friends_count < 50 || tweetJSON.user.statuses_count < 100) { // フォロワー数が50以上かつツイート数が100以上か
         resolve(false);
-        console.log('フォロワー数が50以下もしくはツイート数が100以下のため検証修了');
+        console.info('フォロワー数が50以下もしくはツイート数が100以下のため検証修了');
       } else if (pastRequest) {
         let pastRequestAt = pastRequest.lastRequest;
         let intervalTime = requestAt - pastRequestAt;
@@ -37,9 +37,10 @@ const VerifyUser = function (tweetJSON, requestAt) {
           userRegistration(tweetJSON, requestAt, pastRequest.faucetCount);
           let returnData = [userScreenName, pastRequest.faucetCount];
           resolve(returnData);
-        }
-        console.log('24時間以内にリクエストを行っていたため検証修了');
+        } else {
+        console.info('24時間以内にリクエストを行っていたため検証修了');
         resolve(false);
+        }
       } else if (!pastRequest) {
         userRegistration(tweetJSON, requestAt,0);
         let returnData = [userScreenName, 0];
