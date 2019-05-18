fad5cf15f40837f09a672cba411d70bd562b89ae
ocknamo <s14pes@gmail.com>
2019-05-18 16:05:38 +0900

    fix: update sequelize to 5.8.6

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,8 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('tipnem_faucet', 'postgres', '', {
   dialect: 'postgres',
   logging: false,
-  freezeTableName: true,
-  operatorsAliases: false });
+  freezeTableName: true});
 
 module.exports = {
   database: sequelize,
